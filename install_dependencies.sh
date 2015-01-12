b920b796bebdcf8261508ac4d59c01f3df408665
Hobson Lane <hobsonlane@gmail.com>
2015-01-11 19:30:34 -0800

    all I've done so far is install stuff

---
+++ install_dependencies.sh
@@ -0,0 +1,4 @@
+
+sudo yum install -y sphinxbase-python sphinxbase-devel sphinxtrain pocketsphinx-devel pocketsphinx-python
+sudo yum install -y espeak espeak-devel python-espeak
+curl -L http://downloads.sourceforge.net/project/cmusphinx/Acoustic%20and%20Language%20Models/US%20English%20Generic%20Language%20Model/cmusphinx-5.0-en-us.lm.gz > cmusphinx-5.0-en-us.lm.gz
